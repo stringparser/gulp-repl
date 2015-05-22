9a68d1f8064bcfa081d28e698738b67f65d53285
Javier Carrillo <stringparser@gmail.com>
2015-05-22 17:17:09 +0200

    tests: write tests for new setup

--- test/index.js
+++ test/index.js
@@ -3,17 +3,18 @@
 require('should');
 
 describe('gulp-repl', function(){
-  var gulp = require('../');
+  var gulp = require('gulp');
+  var repl = require('../');
 
   it('repl property that is a readline Interface', function(){
     var readline = require('readline');
-    gulp.repl.constructor.should.be.eql(readline.Interface);
+    repl.constructor.should.be.eql(readline.Interface);
   });
 
   it('should dispatch registered tasks', function(done){
     var one, two;
     gulp.task('one', function(cb){ one = true; cb(); });
     gulp.task('two', function(cb){ two = true; done(); cb(); });
-    gulp.repl.emit('line', 'one two');
+    repl.emit('line', 'one two');
   });
 });
