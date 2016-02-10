5b93ae7a01f98acf1dfcae6ccb72b485d52f17af
Javier Carrillo <stringparser@gmail.com>
2016-02-10 22:58:04 +0100

    tests: make better tests, and make them pass

--- test/index.js
+++ test/index.js
@@ -4,7 +4,7 @@ var should = require('should');
 
 describe('gulp-repl', function(){
   var gulp = require('gulp');
-  var repl = require('../');
+  var repl = require('../')(gulp);
 
   it('repl property that is a readline Interface', function(){
     var readline = require('readline');
@@ -12,17 +12,31 @@ describe('gulp-repl', function(){
   });
 
   it('should dispatch registered tasks', function(done){
-    var one, two;
-    gulp.task('one', function(cb){ one = true; cb(); });
-    gulp.task('two', function(cb){ two = true; done(); cb(); });
+    var pile = [];
+
+    gulp.task('one', function(cb){
+      pile.push('one');
+      if(pile.length > 1){ end(); }
+      cb();
+    });
+    gulp.task('two', function(cb){
+      pile.push('two');
+      if(pile.length > 1){ end(); }
+      cb();
+    });
+
     repl.emit('line', 'one two');
+
+    function end(){
+      pile.should.containDeep(['one', 'two']);
+      done();
+    }
   });
 
   it('undefined tasks should not run', function(){
     gulp.task('one', function(){});
     var tasks = gulp.tasks || gulp._registry._tasks;
     should.exist(tasks);
-
     repl.emit('line', 'whatever');
   });
 });
