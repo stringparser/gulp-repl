1bd02a40127fda1e8516b0f626cc10d67774716b
Javier Carrillo <stringparser@gmail.com>
2016-06-12 19:16:13 +0200

    docs: simpler gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -4,17 +4,13 @@ var gulp = require('gulp');
 gulp.repl = require('./.')(gulp);
 
 gulp.task('foo', function (cb) {
-  // do foo stuff
   setTimeout(cb, Math.random() * 1000);
 });
 
 gulp.task('bar', function (cb) {
-  // do bar stuff
   setTimeout(cb, Math.random() * 500);
 });
 
-gulp.task('watch', function(cb){
+gulp.task('default');
 
-});
-
-gulp.task('default', ['foo', 'bar']);
+gulp.repl.emit('line', 'foo bar');
