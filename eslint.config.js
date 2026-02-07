57cdbc5d598ab0a1360b640b176a0fd82484ceb1
Javier Carrillo Milla <stringparser@gmail.com>
2026-02-07 16:29:41 +0100

    Clear eslint warning

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,7 @@
-import js from '@eslint/js';
-import tseslint from 'typescript-eslint';
+const js = require('@eslint/js');
+const tseslint = require('typescript-eslint');
 
-export default tseslint.config(
+module.exports = tseslint.config(
   js.configs.recommended,
   ...tseslint.configs.recommended,
   {
